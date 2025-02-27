09b50c62ab0deb6e44c51164a2c9e53b0d68b0f1
Florian <45694132+flo-bit@users.noreply.github.com>
2025-02-27 12:20:02 +0100

    small fix

--- src/lib/preview/TextareaPreview.svelte
+++ src/lib/preview/TextareaPreview.svelte
@@ -8,12 +8,12 @@
 
 <Text class="mb-2 mt-4">Primary</Text>
 
-<Textarea placeholder="Enter text here (default)" sizeVariant="sm" />
+<Textarea placeholder="Enter text here (small)" sizeVariant="sm" />
 <Textarea placeholder="Enter text here (default)" />
-<Textarea placeholder="Enter text here (default)" sizeVariant="lg" />
+<Textarea placeholder="Enter text here (large)" sizeVariant="lg" />
 
 <Text class="mb-2 mt-4">Secondary</Text>
 
-<Textarea placeholder="Enter text here (default)" variant="secondary" sizeVariant="sm" />
+<Textarea placeholder="Enter text here (small)" variant="secondary" sizeVariant="sm" />
 <Textarea placeholder="Enter text here (default)" variant="secondary" />
-<Textarea placeholder="Enter text here (default)" variant="secondary" sizeVariant="lg" />
+<Textarea placeholder="Enter text here (large)" variant="secondary" sizeVariant="lg" />
