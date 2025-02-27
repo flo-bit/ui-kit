d23e4d7aaba2843fe25e6a955dcda49aadebce00
Florian <45694132+flo-bit@users.noreply.github.com>
2025-02-27 12:59:52 +0100

    small fix

--- src/lib/preview/Container.svelte
+++ src/lib/preview/Container.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <div class="lg:ml-72 xl:ml-80">
-	<ScrollArea type="always" class="h-[100dvh] w-full" scrollbarYClasses="z-50">
+	<ScrollArea type="scroll" class="h-[100dvh] w-full" scrollbarYClasses="z-50">
 		<div class="mx-auto flex w-full max-w-xl flex-col items-start justify-center gap-2 px-4 py-24">
 			{@render children?.()}
 		</div>
