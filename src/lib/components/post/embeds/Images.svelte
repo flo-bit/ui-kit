c74b0fa16b3bd51c571e1611cd4f3bc8f065d953
Florian <45694132+flo-bit@users.noreply.github.com>
2025-03-02 22:50:04 +0100

    add multi image post

--- src/lib/components/post/embeds/Images.svelte
+++ src/lib/components/post/embeds/Images.svelte
@@ -4,12 +4,15 @@
 	const { data }: { data: PostEmbedImage } = $props();
 </script>
 
-{#snippet imageSnippet(image: {
-	alt: string;
-	thumb: string;
-	fullsize: string;
-	aspectRatio?: { width: number; height: number };
-})}
+{#snippet imageSnippet(
+	image: {
+		alt: string;
+		thumb: string;
+		fullsize: string;
+		aspectRatio?: { width: number; height: number };
+	},
+	className?: string
+)}
 	<img
 		loading="lazy"
 		src={image.thumb}
@@ -17,16 +20,19 @@
 		style={image.aspectRatio
 			? `aspect-ratio: ${image.aspectRatio.width} / ${image.aspectRatio.height}`
 			: 'aspect-ratio: 1 / 1'}
-		class="border-base-500/20 dark:border-base-400/20 w-full rounded-2xl border object-cover"
+		class={[
+			'border-base-500/20 dark:border-base-400/20 w-full rounded-2xl border object-cover',
+			className
+		]}
 	/>
 {/snippet}
 
 {#if data.images.length === 1}
 	{@render imageSnippet(data.images[0])}
 {:else}
-	<div class="grid grid-cols-2 gap-4">
+	<div class="columns-2 gap-4">
 		{#each data.images as image}
-			{@render imageSnippet(image)}
+			{@render imageSnippet(image, 'mb-4')}
 		{/each}
 	</div>
 {/if}
--- src/lib/preview/PostPreview.svelte
+++ src/lib/preview/PostPreview.svelte
@@ -9,8 +9,36 @@
 	let blueskyPosts: PostData[] | null = $derived(
 		data.posts.map((post: unknown) => {
 			return blueskyPostToPostData(post);
-		})
+		}).reverse()
 	);
+
+	// onMount(async () => {
+	// 	let uris = [
+	// 		'at://did:plc:257wekqxg4hyapkq6k47igmp/app.bsky.feed.post/3lj3hqgiynk2v',
+	// 		'at://did:plc:257wekqxg4hyapkq6k47igmp/app.bsky.feed.post/3lj6idw2ibc22',
+	// 		'at://did:plc:257wekqxg4hyapkq6k47igmp/app.bsky.feed.post/3lj6hwahre222',
+	// 		'at://did:plc:257wekqxg4hyapkq6k47igmp/app.bsky.feed.post/3liu4v5sa7s2a',
+	// 		'at://did:plc:257wekqxg4hyapkq6k47igmp/app.bsky.feed.post/3ljgfoyheyc2b'
+	// 	];
+	// 	const params = new URLSearchParams();
+	// 	uris.forEach((uri) => params.append('uris', uri));
+
+	// 	const response = await fetch(
+	// 		`https://public.api.bsky.app/xrpc/app.bsky.feed.getPosts?${params.toString()}`
+	// 	);
+
+	// 	const data = await response.json();
+
+	// 	// download data as json
+	// 	const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
+	// 	const url = URL.createObjectURL(blob);
+	// 	const a = document.createElement('a');
+	// 	a.href = url;
+	// 	a.download = 'data.json';
+	// 	a.click();
+
+	// });
+
 
 </script>
 
