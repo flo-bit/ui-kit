d05335b85930e51b25b8b7db2fda259f4de34a3e
Florian <45694132+flo-bit@users.noreply.github.com>
2025-03-22 04:26:47 +0100

    imrpove swiper

--- src/lib/components/extra/swiper-cards/CardSwiper.svelte
+++ src/lib/components/extra/swiper-cards/CardSwiper.svelte
@@ -1,46 +1,97 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	// TODO: fix types
+	
+	import { onMount, type Snippet } from 'svelte';
 	import { DragGesture, type FullGestureState } from '@use-gesture/vanilla';
 	import type { CardData, Direction, SwipeEventData } from '.';
-	import Card from './Card.svelte';
+	import { Image } from '$lib';
+
+	let {
+		card = DefaultCard,
+		cardData,
+		onswipe,
+		swipe = $bindable(),
+
+		minSwipeDistance = 0.5,
+		minSwipeVelocity = 0.5,
+		arrowKeys = true,
+		thresholdPassed = $bindable(0),
+		anchor = null,
+		rotate = true,
+		cardCount = 10
+	}: {
+		card?: Snippet<[CardData]>;
+
+		cardData: (index: number) => CardData;
+		onswipe?: (cardInfo: SwipeEventData) => void;
+		swipe?: (direction: Direction) => void;
+
+		minSwipeDistance?: number;
+		minSwipeVelocity?: number;
+		arrowKeys?: boolean;
+		thresholdPassed?: number;
+		anchor?: number | null;
+		rotate?: boolean;
+		cardCount?: number;
+	} = $props();
+
+	swipe = (direction: Direction = 'right') => {
+		if (thresholdPassed !== 0) return;
+
+		let dir = direction === 'left' ? -1 : 1;
+
+		if (!topCard) throw new Error('No top card found');
+		cardSwiped(topCard, [dir, 0.1], [dir, 1]);
+	};
 
 	let container: HTMLElement;
 
-	let card1: HTMLElement | undefined = $state();
-	let card2: HTMLElement | undefined = $state();
-	let card1Data: CardData = $state({});
-	let card2Data: CardData = $state({});
-
 	let cardIndex = 0;
 	let topCard: HTMLElement | undefined = $state();
+	let topCardIndex = 0;
+
 	let currentZ = 100000;
 
-	onMount(async () => {
-		card1Data = cardData(cardIndex++);
-		card2Data = cardData(cardIndex++);
+	let cards: HTMLElement[] = $state(new Array(cardCount));
+	let cardsData: CardData[] = $state([]);
 
-		[card1, card2].forEach((el) => {
+	onMount(() => {
+		for (let i = 0; i < cardCount; i++) {
+			cardsData.push(cardData(cardIndex++));
+			cards.push();
+		}
+
+		let gestures: DragGesture[] = [];
+
+		cards.forEach((el) => {
 			if (!el) return;
+
 			el.style.zIndex = currentZ.toString();
 			currentZ--;
 
-			new DragGesture(
-				el,
-				(state) => {
-					ondrag(el, state);
-				}
-			);
+			let gesture = new DragGesture(el, (state) => {
+				ondrag(el, state);
+			});
+
+			gestures.push(gesture);
 		});
 
-		topCard = card1;
+		topCardIndex = 0;
+		topCard = cards[topCardIndex];
+
 		container.classList.remove('hidden');
+
+		return () => {
+			gestures.forEach((gesture) => gesture.destroy());
+		};
 	});
 
 	const cardSwiped = (el: HTMLElement, velocity: [number, number], movement: [number, number]) => {
 		el.classList.add('transition-transform', 'duration-300');
+		let index = cards.indexOf(el);
 
 		let direction: Direction = movement[0] > 0 ? 'right' : 'left';
-		let data = el === card1 ? card1Data : card2Data;
+		let data = cardsData[index];
 
 		onswipe?.({ direction, element: el, data, index: cardIndex - 2 });
 
@@ -57,19 +108,14 @@
 
 		el.style.transform = `translate(${toX}px, ${toY + movement[1]}px) rotate(${rotate}deg)`;
 
-		setTimeout(() => {
+		setTimeout(async () => {
 			thresholdPassed = 0;
 
-			// move card back to start position at bottom of stack and update data
-			if (el === card1) {
-				card1Data = {};
-				card1Data = cardData(cardIndex++);
-				topCard = card2;
-			} else {
-				card2Data = {};
-				card2Data = cardData(cardIndex++);
-				topCard = card1;
-			}
+			cardsData[index] = cardData(cardIndex++);
+
+			// next card
+			topCardIndex = (topCardIndex + 1) % cardCount;
+			topCard = cards[topCardIndex];
 
 			currentZ--;
 			el.style.zIndex = currentZ.toString();
@@ -129,41 +175,10 @@
 			cardSwiped(el, state.velocity, state.movement);
 		}
 	};
-
-	let {
-		cardData,
-		minSwipeDistance = 0.5,
-		minSwipeVelocity = 0.5,
-		arrowKeys = true,
-		thresholdPassed = $bindable(0),
-		anchor = null,
-		onswipe,
-		swipe = $bindable(),
-		rotate = true,
-	}: {
-		cardData: (index: number) => CardData;
-		minSwipeDistance?: number;
-		minSwipeVelocity?: number;
-		arrowKeys?: boolean;
-		thresholdPassed?: number;
-		anchor?: number | null;
-		onswipe?: (cardInfo: SwipeEventData) => void;
-		swipe?: (direction: Direction) => void;
-		rotate?: boolean;
-	} = $props();
-
-	swipe = (direction: Direction = 'right') => {
-		if (thresholdPassed !== 0) return;
-
-		let dir = direction === 'left' ? -1 : 1;
-
-		if (!topCard) throw new Error('No top card found');
-		cardSwiped(topCard, [dir, 0.1], [dir, 1]);
-	};
 </script>
 
 <svelte:body
-	on:keydown={(e) => {
+	onkeydown={(e) => {
 		if (!arrowKeys) return;
 		if (e.key === 'ArrowLeft') {
 			swipe('left');
@@ -173,9 +188,48 @@
 	}}
 />
 
+{#snippet DefaultCard({ image, title, description }: CardData)}
+	<div
+		class="border-base-400 dark:border-base-600 bg-base-50 dark:bg-base-900 relative h-full w-full overflow-hidden rounded-2xl border"
+	>
+		{#key image}
+			{#if image}
+				<Image
+				containerClasses="absolute inset-0 h-full w-full rounded-2xl"
+					src={image}
+					alt={title ?? ''}
+					loading="eager"
+					class="h-full w-full object-cover"
+				/>
+			{/if}
+		{/key}
+		<div
+			class="from-base-50/80 dark:from-base-950/80 absolute inset-0 rounded-b-xl bg-gradient-to-t via-transparent"
+		></div>
+		<div class="absolute bottom-0 flex w-full justify-start px-3 sm:px-12 py-16">
+			<div class="flex flex-col">
+				<h3 class="text-base-900 dark:text-base-50 pb-2 text-3xl font-bold">{title}</h3>
+				<p class="text-base-800 dark:text-base-200 text-sm">{description}</p>
+			</div>
+		</div>
+	</div>
+{/snippet}
+
 <div class="isolate h-full w-full touch-none select-none">
 	<div class="relative z-0 hidden h-full w-full" bind:this={container}>
-		<Card bind:ref={card1} {...card1Data} />
-		<Card bind:ref={card2} {...card2Data} />
+		{#each cards as _, i}
+			<div
+				bind:this={cards[i]}
+				class="absolute h-full w-full cursor-grab touch-none overflow-hidden ease-in-out select-none"
+			>
+				{#if cardsData[i]}
+					{@render card({
+						image: cardsData[i].image,
+						title: cardsData[i].title,
+						description: cardsData[i].description
+					})}
+				{/if}
+			</div>
+		{/each}
 	</div>
 </div>
