05ec15ece76189d50cb13998b224a4ad292dbf18
Florian <45694132+flo-bit@users.noreply.github.com>
2025-03-28 12:33:17 +0100

    update cli

--- src/lib/cli.ts
+++ src/lib/cli.ts
@@ -1,50 +1,48 @@
 #!/usr/bin/env node
 
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import fs from 'node:fs';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import components from './components.json' with { type: 'json' };
+
 import fetch from 'node-fetch';
 import AdmZip from 'adm-zip';
-import components from './components.json' with { type: 'json' };
+import { spinner, log } from '@clack/prompts';
+import { Command } from 'commander';
+import { version } from '../../package.json' with { type: 'json' };
+
+const program = new Command();
+
+const s = spinner();
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-
-const args = process.argv.slice(2);
-if (args.length === 0) {
-	console.error('Usage: cli <component-name>');
-	process.exit(1);
-}
-
-const componentName = args[0].toLowerCase();
-if (!components[componentName as keyof typeof components]) {
-	console.error(`Component "${componentName}" not found in components.json`);
-	process.exit(1);
-}
-
-const relativePath = components[componentName as keyof typeof components];
-
 const githubUser = 'flo-bit';
 const repoName = 'ui-kit';
 const branch = 'main';
 const zipUrl = `https://github.com/${githubUser}/${repoName}/archive/refs/heads/${branch}.zip`;
-
 const tmpDir = path.join(__dirname, 'tmp');
-if (!fs.existsSync(tmpDir)) {
-	fs.mkdirSync(tmpDir);
-}
+const zipPath = path.join(tmpDir, `repo-${version}.zip`);
 
-const zipPath = path.join(tmpDir, 'repo.zip');
+program.name('fuchs').description('CLI to install fox UI components').version(version);
+
+program
+	.command('add')
+	.description('Add a new component to the project')
+	.argument('<component-name>', 'name of the component to add')
+	.action((component) => {
+		addComponent(component);
+	});
+
+program.parse();
 
 async function downloadZip(url: string, dest: string) {
-	console.log(`Downloading ${url} ...`);
 	const response = await fetch(url);
 	if (!response.ok) {
-		throw new Error(`Failed to download ZIP: ${response.statusText}`);
+		throw new Error(`Failed to download repository: ${response.statusText}`);
 	}
 	const buffer = await response.arrayBuffer();
 	fs.writeFileSync(dest, Buffer.from(buffer));
-	console.log('Download complete.');
 }
 
 function copyRecursive(src: string, dest: string) {
@@ -68,53 +66,78 @@ function replaceInFiles(dir: string, searchStr: string, replaceStr: string) {
 		const fullPath = path.join(dir, entry.name);
 		if (entry.isDirectory()) {
 			replaceInFiles(fullPath, searchStr, replaceStr);
-		} else {
-			try {
-				const content = fs.readFileSync(fullPath, 'utf8');
-				if (content.includes(searchStr)) {
-					const newContent = content.split(searchStr).join(replaceStr);
-					fs.writeFileSync(fullPath, newContent, 'utf8');
-					console.log(`Updated ${fullPath}`);
-				}
-			} catch (err) {
-				console.error('Error:', (err as Error).message);
+			continue;
+		}
+		try {
+			const content = fs.readFileSync(fullPath, 'utf8');
+			if (content.includes(searchStr)) {
+				const newContent = content.replaceAll(searchStr, replaceStr);
+				fs.writeFileSync(fullPath, newContent, 'utf8');
 			}
+		} catch (err) {
+			console.error('Error:', (err as Error).message);
 		}
 	}
 }
 
-try {
+async function downloadRepo() {
+	s.start('Downloading repository...');
+	const tmpDir = path.join(__dirname, 'tmp');
+	if (!fs.existsSync(tmpDir)) {
+		fs.mkdirSync(tmpDir);
+	}
+
 	await downloadZip(zipUrl, zipPath);
 
-	console.log('Extracting ZIP archive...');
 	const zip = new AdmZip(zipPath);
-	const extractDir = path.join(tmpDir, 'extract');
+	const extractDir = path.join(tmpDir, `extract-${version}`);
 	if (!fs.existsSync(extractDir)) {
 		fs.mkdirSync(extractDir);
 	}
 	zip.extractAllTo(extractDir, true);
-	console.log('Extraction complete.');
 
-	const packageFolder = path.join(extractDir, `${repoName}-${branch}`);
+	s.stop('Repository downloaded');
+	return extractDir;
+}
+
+async function addComponent(componentName: string) {
+	if (!components[componentName as keyof typeof components]) {
+		console.error(`Component "${componentName}" not found in components.json`);
+		process.exit(1);
+	}
+	const relativePath = components[componentName as keyof typeof components];
+
+	let packageFolder = '';
+	// check if repository is already downloaded
+	const tmpDir = path.join(__dirname, 'tmp');
+	if (!fs.existsSync(tmpDir)) {
+		fs.mkdirSync(tmpDir);
+	}
+	const extractDir = path.join(tmpDir, `extract-${version}`);
+	if (!fs.existsSync(extractDir)) {
+		packageFolder = await downloadRepo();
+	} else {
+		log.info('Repository already downloaded');
+		packageFolder = extractDir;
+	}
+	packageFolder = path.join(packageFolder, `${repoName}-${branch}`);
+
 	const sourcePath = path.join(packageFolder, 'src', 'lib', relativePath);
 	if (!fs.existsSync(sourcePath)) {
-		console.error(`Component folder "${relativePath}" not found in repository ${repoName}`);
+		console.error(`Component folder "${relativePath}" not found in repository`);
 		process.exit(1);
 	}
 
-	const destDir = path.join(process.cwd(), 'src', 'lib', 'ui', componentName);
+	const destDir = path.join(process.cwd(), 'src', 'lib', 'fuchs', componentName);
 	if (!fs.existsSync(destDir)) {
 		fs.mkdirSync(destDir, { recursive: true });
 	}
 
 	copyRecursive(sourcePath, destDir);
-	console.log(`Component "${componentName}" successfully copied to ${destDir}`);
+	log.success(`Component "${componentName}" successfully copied to src/lib/fuchs`);
 
 	replaceInFiles(destDir, '$lib', 'fuchs');
 
 	// Optionally, clean up temporary files
 	// fs.rmSync(tmpDir, { recursive: true, force: true });
-} catch (err) {
-	console.error('Error:', (err as Error).message);
-	process.exit(1);
 }
